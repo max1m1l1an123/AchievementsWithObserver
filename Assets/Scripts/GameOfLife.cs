08a44f1ccae78708a111bf6a78f1fd0b86936c1d
Max Einolander <91366233+max1m1l1an123@users.noreply.github.com>
2023-11-26 18:45:59 +0200

    game of life except it works but soon will have double buffering

--- Assets/Scripts/GameOfLife.cs
+++ Assets/Scripts/GameOfLife.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOfLife : MonoBehaviour
@@ -10,97 +12,52 @@ public class GameOfLife : MonoBehaviour
 
     private float TimeAccu = 0.0f;
 
+    // Start is called before the first frame update
     void Start()
     {
+        // Generate tiles
         for (int x = 0; x < Width; x++)
         {
             for (int y = 0; y < Height; y++)
             {
+                // Clear the grid
                 grid[x, y] = false;
-
+                // Instantiate the tile
                 tiles[x, y] = Instantiate(TileObject,
-                                        new Vector3(x, 0, y) * 1.05f,
+                                        new Vector3(x, 0f, y) * 1.05f,
                                         TileObject.transform.rotation);
-
                 //tiles[x, y].SetActive(false);
                 tiles[x, y].GetComponent<MeshRenderer>().material.color = Color.black;
             }
         }
 
-        grid[5,5] = true;
-        tiles[5,5].GetComponent<MeshRenderer>().material.color = Color.black;
+        grid[5, 5] = true;
+        tiles[5, 5].GetComponent<MeshRenderer>().material.color = Color.red;
         grid[3, 5] = true;
-        tiles[3,5].GetComponent<MeshRenderer>().material.color = Color.black;
-        grid[4, 5] = true;
-        tiles[4, 5].GetComponent<MeshRenderer>().material.color = Color.black;
-        grid[2, 4] = true;
-        tiles[2, 4].GetComponent<MeshRenderer>().material.color = Color.black;
+        tiles[3, 5].GetComponent<MeshRenderer>().material.color = Color.red;
+        grid[4, 4] = true;
+        tiles[4, 4].GetComponent<MeshRenderer>().material.color = Color.red;
+        grid[1, 4] = true;
+        tiles[1, 4].GetComponent<MeshRenderer>().material.color = Color.red;
+        grid[2, 3] = true;
+        tiles[2, 3].GetComponent<MeshRenderer>().material.color = Color.red;
+        grid[1, 3] = true;
+        tiles[1, 3].GetComponent<MeshRenderer>().material.color = Color.red;
+        grid[2, 2] = true;
+        tiles[2, 2].GetComponent<MeshRenderer>().material.color = Color.red;
+
+
+
     }
 
-    void Update()
-    {
-        TimeAccu += Time.deltaTime;
-
-        if (TimeAccu > 2.0f)
-        {
-            // RULES:
-            // 1. fewer than 2 live neighbours --> die
-            // 2. 2 or 3 live neighbours --> live on
-            // 3. more than 3 live neighbours --> die
-            // 4. dead cell with exactly 3 live neighbours --> rebirth
-
-            for (int x = 0; x < Width; x++)
-            {
-                for (int y = 0; y < Height; y++)
-                {
-                    int live = GetLiveNeighbours(x, y);
-
-                    if (live < 2)
-                    {
-                        grid[x, y] = false; // die
-                    }
-                    else if (live < 4 && grid[x, y] == true)
-                    {
-                        // live on.. do nothing
-                    }
-                    else if (live > 3 && grid[x, y] == true)
-                    {
-                        grid[x, y] = false; // die
-                    }
-                    else if (live == 3 && grid[x, y] == false)
-                    {
-                        grid[x, y] = true; // rebirth
-                    }
-                }
-            }
-
-            // Update the tiles
-            for (int x = 0; x < Width; x++)
-            {
-                for (int y = 0; y < Height; y++)
-                {
-                    if (grid[x,y] == true)
-                    {
-                        tiles[x, y].GetComponent<MeshRenderer>().material.color = Color.red;
-                    }
-                    else
-                    {
-                        tiles[x, y].GetComponent<MeshRenderer>().material.color = Color.black;
-                    }
-                }
-            }
-        }
-    }
-
-    int GetLiveNeighbours(int x, int y)
+    private int GetLiveNeighbours(int x, int y)
     {
         int liveneighbours = 0;
-
-        for (int i = x - 1; 1 <= x + 1; i++)
+        for (int i = x - 1; i <= x + 1; i++)
         {
-            for (int j = y - 1; 1 <= y + 1; j++)
+            for (int j = y - 1; j <= y + 1; j++)
             {
-                if (!(i == x && j == y) && i >= 0 && j >= 0 && i < Width && j < Height)
+                if (!(i == x & j == y) && i >= 0 && j >= 0 && i < Width && j < Height)
                 {
                     // current i,j is not x,y
                     if (grid[i, j] == true)
@@ -110,7 +67,62 @@ public class GameOfLife : MonoBehaviour
                 }
             }
         }
-
         return liveneighbours;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeAccu += Time.deltaTime;
+
+        if (TimeAccu > 2.0f)
+        {
+            // RULES:
+            // 1. Fewer than 2 live neighbours --> die
+            // 2. 2 or 3 live neighbours -> live on
+            // 3. more than 3 live neighbours -> die
+            // 4. dead cell with 3 live neighbours -> rebirth 
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    int live = GetLiveNeighbours(x, y);
+                    if (live < 2)
+                    {
+                        grid[x, y] = false;
+                    }
+                    else if (live < 4 && grid[x, y] == true)
+                    {
+                        // live on... do nothing
+                    }
+                    else if (live > 3 && grid[x, y] == true)
+                    {
+                        grid[x, y] = false;
+                    }
+                    else if (live == 3 && grid[x, y] == false)
+                    {
+                        grid[x, y] = true;
+                    }
+                }
+            }
+
+            // Update the tiles
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    if (grid[x, y] == true)
+                    {
+                        tiles[x, y].GetComponent<MeshRenderer>().material.color = Color.red;
+                    }
+                    else
+                    {
+                        tiles[x, y].GetComponent<MeshRenderer>().material.color = Color.black;
+                    }
+                }
+            }
+
+            TimeAccu = 0.0f;
+        }
     }
 }
